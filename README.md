f0b4a097955b7bad64d2f10436a744c6be4f4812
Rambou <Rambou@users.noreply.github.com>
2022-09-28 11:44:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# https-github.com-ec-europa-eui-sb-logService-Initiate
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-i6i4fc)
---
+++ src/config/global.ts
@@ -0,0 +1,22 @@
+import { GlobalConfig } from '@eui/core';
+import { environment } from '../environments/environment';  
+
+export const GLOBAL: GlobalConfig = {
+    appTitle: 'CSDR-app',
+    i18n: {
+        i18nService: {
+            defaultLanguage: 'en',
+            languages: ['en', 'fr'],
+        },
+        i18nLoader: {
+            i18nFolders: ['i18n'],
+            i18nServices: [`${environment.apiBaseUrl}translations?lang=`] 
+        },
+    },
+    user: {
+        defaultUserPreferences: {
+            dashboard: { },
+            lang: 'en',
+        },
+    },
+};
